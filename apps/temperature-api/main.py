9f8bb5f921cda7c90f08a6de67702ebcabb8736f
KalantarianVA <KalantarianVA@mosreg.ru>
2026-02-04 09:59:10 +0300

    add docker-compose.yml

--- apps/temperature-api/main.py
+++ apps/temperature-api/main.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Path
 import random
 
 app = FastAPI()
 @app.get("/temperature")
 def temperature(location: str = Query(..., description="Location")):
-    temperature_now = round(random.uniform(0, 40), 1)
-    return temperature_now
+    return random.randint(0, 50)
+
+@app.get("/temperature/{location}")
+def temperature_2(location: str = Path(..., description="Location")):
+    return {"value": random.randint(0, 50)}
